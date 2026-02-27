0069a26a5593a851f5ce35e15083bd70918fb2bd
Horacio <balderashoracio93@gmail.com>
2026-02-27 17:13:10 -0600

    Import de las fuentes

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import '@fontsource/montserrat/400.css';
+import '@fontsource/playfair-display/700.css'; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
